ff916b95b8488c3c83f7553e813f5ceb532e90c2
Daniel <danielzwang97@gmail.com>
2022-08-20 20:23:10 -0700

    Complete program

---
+++ script.rb
@@ -0,0 +1,18 @@
+## dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+## str = "Howdy partner, sit down! How's it going?"
+
+def substrings(string, dictionary)
+  string = string.downcase
+  str_array = string.split(" ")
+  did = dictionary.reduce(Hash.new(0)) do |result, dictionaryword|
+    str_array.each do |word|
+      if word.include? dictionaryword
+        result[dictionaryword] += 1
+      end
+    end
+  result
+  end
+end
+
+p did
